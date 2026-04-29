396ea9100a212f87909e8b7fe0d3897848d4dec5
Marcelo Gaspar <epilorca@gmail.com>
2026-04-29 15:55:58 -0300

    Atualizando kpi.py

--- kpi.py
+++ kpi.py
@@ -1,5 +1,19 @@
+CONSTANTE_BONUS = 1000
+# 1 - Solicita ao usuário que digite seu nome
 nome = input("Digite o seu nome: ")
+
+# 2 - Solicita ao usuário que digite o valor do salário 
+# Converta o valor de entrada em ponto flutuante
 salario = float(input("Digite o valor bruto do seu salário: "))
+
+# 3 - Solicita ao usuário que digite o valor do bônus recebido 
+# Converta o valor de entrada em ponto flutuante
 bonus = float(input("Digite o valor do bônus recebido: "))
-calc = 1000 + (salario * bonus)
-print("Olá, ", nome,"O seu bônus foi de, R$ ", calc)
+
+# 4 - Calcule o valor do bonus final
+calc = CONSTANTE_BONUS + (salario * bonus)
+
+# 5 - Imprima o cálculo do KPI para o usuário
+print("Olá, ", nome,"O seu bônus foi de R$ ", calc)
+
+
